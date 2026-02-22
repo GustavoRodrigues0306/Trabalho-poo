0e3966f0ec684dbacf562335d0f151a6cee5b778
Kelvin Fagundes <kelvin.desouza@dcomp.ufs.br>
2026-02-22 18:01:30 -0300

    Adicionando classe aluno e comentarios ao codigo

--- main.cpp
+++ main.cpp
@@ -6,56 +6,80 @@
 using namespace std;
 
 
-/* =========================
-CLASSE BASE (HERANÇA)
-=========================*/
+// CLASSE BASE (HERANCA)
 
 class Pessoa{
 
 protected:
 
-string nome;
+    string nome;
 
 public:
 
-Pessoa(string n)
-: nome(n){}
+    Pessoa(string n) : nome(n){}
 
-virtual void render() const=0;
+    // Metodo Virtual Puro: define a interface obrigatoria para as filhas
+    virtual void render() const=0;
 
-virtual ~Pessoa(){}
+    // Destrutor Virtual: garante a limpeza correta em subclasses
+    virtual ~Pessoa(){}
 
 };
-/*
-=========================
-CLASSE PROFESSOR
-=========================*/
 
-class Professor:public Pessoa{
+// CLASSE ALUNO
+
+    // Heranca: Aluno estende Pessoa
+class Aluno:public Pessoa{
 
 private:
 
-string departamento;
+    string matricula;
+
+    vector<float> notas;
 
 public:
 
-Professor(string n,
-string d)
+    Aluno(string n, string m) :Pessoa(n), matricula(m){
+    notas.resize(4);
+    }
 
-:Pessoa(n),
-departamento(d){}
+    // Encapsulamento: validacao de dados de entrada
+    void setNota(int i,float n){
+        if(n<0||n>10) throw invalid_argument("Nota invalida");
+        notas[i]=n;
+    }
 
 
-void render() const override{
+    float media() const{
+        float soma=0;
+        for(float n:notas) soma+=n;
+        return soma/4;
+    }
 
-cout<<"Professor:"
-<<nome
 
-<<" Dept:"
-<<departamento
+    string getMatricula() const{
+        return matricula;
+    }
 
-<<endl;
+    // Polimorfismo: implementacao especifica para Aluno
+    void render() const override{
+        cout<<"Aluno:" <<   nome <<" Mat:" << matricula << " Media:" << media() << endl;
+    }
 
-}
+};
 
+//CLASSE PROFESSOR
+
+// Heranca: Professor estende Pessoa
+class Professor:public Pessoa{
+private:
+    string departamento;
+
+public:
+
+    Professor(string n, string d) :Pessoa(n), departamento(d){}
+
+    // Polimorfismo: implementacao especifica para Professor
+    void render() const override{ cout << "Professor:" << nome << " Dept:" << departamento << endl;
+    }
 };
